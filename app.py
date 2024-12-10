bbbd5dba5722f58b2bb3a9614de9a50ed122d45e
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-10 15:34:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,10 @@
-from flask import Flask, send_from_directory
+from flask import Flask
 
 app = Flask(__name__)
 
-# Route to serve Swagger UI
-@app.route('/swagger-ui')
-def swagger_ui():
-    return send_from_directory('static', 'swagger-ui.html')
+@app.route('/health')
+def health():
+    return 'OK', 200
 
-# Route for the API
-@app.route('/api')
-def api():
-    return "API is up!"
-
-# Start the Flask application
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8000)
+if __name__ == '__main__':
+    app.run(debug=True)
