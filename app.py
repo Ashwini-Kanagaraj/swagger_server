b1e07bd1b97ca120b2a094210bc78224c5cc3b6b
Ashwini-Kanagaraj <159413670+Ashwini-Kanagaraj@users.noreply.github.com>
2024-12-10 15:37:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/health')
-def health():
-    return 'OK', 200
+def health_check():
+    return 'OK', 200  # A simple response indicating the app is healthy
 
 if __name__ == '__main__':
     app.run(debug=True)
